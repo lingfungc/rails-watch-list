ca1c19762dba1b73410d52be8016e94f9c852f4c
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-21 17:15:32 +0000

    updated seed.rb with using API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning up database'
+Movie.destroy_all
+puts 'Cleaned Database'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+# 10.times do |i|
+
+puts 'Importing movies'
+movies = JSON.parse(URI.open(url).read)['results']
+
+movies.each do |movie|
+  puts "Creating movie #{movie['title']}"
+  base_movie_url = 'https://image.tmdb.org/t/p/w500'
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{base_movie_url}#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'Created movies'
+
+# end
